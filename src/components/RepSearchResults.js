e124011e1a9475a6e21ce35aeeea94b17cbed812
chrisdeleonn <cdizzel@ymail.com>
2021-07-26 16:42:04 -0400

    revised

--- src/components/RepSearchResults.js
+++ src/components/RepSearchResults.js
@@ -5,13 +5,13 @@ function RepSearchResults(props) {
     <div>
       {offices.map((office) => {
         const official = officials[office.officialIndices[0]]
-        const address = official?.address && official?.address[0]
         return (
           <div className='rep-info'>
             <div className='office-title'>{office.name}</div>
             <p>{official?.name}</p>
             <label>
-              Phone:&nbsp;<span>{official?.phones[0]}</span>
+              Phone:&nbsp;
+              <a href={`tel:${official?.phones[0]}`}></a>
             </label>
             <br />
             {official?.address && (
@@ -19,17 +19,32 @@ function RepSearchResults(props) {
                 <label>
                   Address:&nbsp;
                   <span>
-                    {address?.line1}, &nbsp;{address?.city}, &nbsp;{address?.state}, &nbsp;{address?.zip}
+                    {official?.address[0]?.line1}, &nbsp;{official?.address[0]?.city}, &nbsp;{official?.address[0]?.state}, &nbsp;
+                    {official?.address[0]?.zip}
                   </span>
                 </label>
                 <br />
               </>
             )}
-
             {official?.emails && (
-              <label>
-                Email:&nbsp;<span>{official?.emails[0]}</span>
-              </label>
+              <>
+                <label>
+                  Email:&nbsp;
+                  <a href={`mailto:${official?.emails[0]}`}>{official?.emails[0]}</a>
+                </label>
+                <br />
+              </>
+            )}
+            {official?.urls && (
+              <>
+                <label>
+                  Website:&nbsp;
+                  <a href={official?.urls[0]} target='_blank'>
+                    {official?.urls[0]}
+                  </a>
+                </label>
+                <br />
+              </>
             )}
           </div>
         )
