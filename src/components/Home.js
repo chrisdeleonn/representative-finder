a1b901043a7e034d40e02ee116b964ef1892b3c1
chrisdeleonn <cdizzel@ymail.com>
2021-08-07 12:55:18 -0400

    refactored

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,9 @@
 const Home = () => {
   return (
     <div className='home-page'>
-      <p>
-        If you've ever wondered what your local government tree hierarchy looks like during the voting terms, now you easily can, with this. Just
-        enter your physical address, and the list will generate, all the way up to the President! Pretty cool, right?
+      <p className='home-text'>
+        If you've ever wondered who your local representatives are... now you easily can, with this tool. Just enter your physical address, and the
+        list will generate, all the way up to the President! Pretty cool, right?
       </p>
       <br />
     </div>
